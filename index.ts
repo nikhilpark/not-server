86b09b583d4203385124467f29f8882368a76f30
nikhilpark <nikhil.k@sheru.se>
2022-01-16 01:01:13 +0530

    .

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express,{Request,Response} from 'express'
  const app:express.Application = express()
-const port = 6969;
+const port = 80;
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}`);
 })
