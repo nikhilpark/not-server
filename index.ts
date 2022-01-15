6de958d6929ff40a5129e649886526067b322662
nikhilpark <nikhil.k@sheru.se>
2022-01-16 00:51:42 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express,{Request,Response} from 'express'
+ const app:express.Application = express()
+const port = 6969;
+app.listen(port,()=>{
+    console.log(`App is listening on port ${port}`);
+})
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('This app actually worrks!')
+})
